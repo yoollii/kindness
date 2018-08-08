6128ddb4110e7758d1cf78b059a79451fbd3785c
yoollii <yoollii@github.com>
2018-08-08 09:29:08 +0800

    图表

--- app/home/antv/antv.component.ts
+++ app/home/antv/antv.component.ts
@@ -10,6 +10,8 @@ export class AntvComponent implements OnInit {
   title = 'app';
   data = {};
   chart ;
+  data2 = {};
+  chart2;
   graph;
   constructor() { }
   ngOnInit() {
@@ -23,16 +25,70 @@ export class AntvComponent implements OnInit {
       { genre: 'Shooter', sold: 350 },
       { genre: 'Other', sold: 150 }
     ];
+    this.data2 = [{
+      item: '待分解',
+      count: 60,
+      percent: 0.6
+    }, {
+      item: '待审批',
+      count: 20,
+      percent: 0.20
+    }, {
+      item: '正在执行',
+      count: 10,
+      percent: 0.1
+    }, {
+      item: '已完成',
+      count: 10,
+      percent: 0.10
+    }];
      this.chart = new G2.Chart({
       container: 'c1', // 指定图表容器 ID
       width : 600, // 指定图表宽度
       height : 300 // 指定图表高度
     });
+    this.chart2 = new G2.Chart({
+      container: 'c2',
+      forceFit: true,
+      width : 600, // 指定图表宽度
+      height : 300 // 指定图表高度
+    });
 
     this.chart.source(this.data);
+    this.chart2.source(this.data2, {
+      percent: {
+        formatter: function formatter(val) {
+          debugger;
+          val = val * 100 + '%';
+          return val;
+        }
+      }
+    });
+    this.chart2.coord('theta', {
+      radius: 0.75
+    });
+    this.chart2.tooltip({
+      showTitle: false,
+      itemTpl: '<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
+    });
+    this.chart2.intervalStack().position('percent').color('item').label('percent', {
+      formatter: function formatter(val, item) {
+        return item.point.item + ': ' + val;
+      }
+    }).tooltip('item*percent', function (item, percent) {
+      percent = percent * 100 + '%';
+      return {
+        name: item,
+        value: percent
+      };
+    }).style({
+      lineWidth: 1,
+      stroke: '#fff'
+    });
     this.chart.interval().position('genre*sold').color('genre');
     //  渲染图表
     this.chart.render();
+    this.chart2.render();
   }
 
 }
