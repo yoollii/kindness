419ad1594cb6b3f2fb41005695b90db17b9db4a2
yoollii <yoollii@github.com>
2018-08-10 09:29:45 +0800

    修改首页

--- app/home/home-page/home-page.component.ts
+++ app/home/home-page/home-page.component.ts
@@ -6,6 +6,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   private hide: Boolean = false;
+  panels = [
+    {
+      active: true,
+      disabled: false,
+      name: '我的任务',
+      data1: '完成**运输',
+      data2: '前往**部门报道',
+      customStyle: {
+        'background': '#f7f7f7',
+        'border-radius': '4px',
+        'margin-bottom': '24px',
+        'border': '0px'
+      }
+    },
+    {
+      active: false,
+      disabled: true,
+      name: '消息列表',
+      data1: '**设备更新',
+      data2: '今天建军节',
+      customStyle: {
+        'background': '#f7f7f7',
+        'border-radius': '4px',
+        'margin-bottom': '24px',
+        'border': '0px'
+      }
+    },
+    {
+      active: false,
+      disabled: false,
+      name: '操作日志',
+      data1: '新增',
+      data2: '删除',
+      customStyle: {
+        'background': '#f7f7f7',
+        'border-radius': '4px',
+        'margin-bottom': '24px',
+        'border': '0px'
+      }
+    }
+  ];
   constructor() {}
   gridStyle = {
     width: '100%',
